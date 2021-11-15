66d7de0754888c0cde06c63aafc4c3f8cc5c7f22
Akanksha Baishwade <85228361+akanshabaishwade@users.noreply.github.com>
2021-11-15 16:00:42 +0530

    Update settings.py

--- django_project_1/django_project_1/settings.py
+++ django_project_1/django_project_1/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
 
+    
+    
+    
 ]
 
 MIDDLEWARE = [
