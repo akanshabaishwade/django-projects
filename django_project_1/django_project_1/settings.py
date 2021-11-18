efcb2a374918dead6527112a3fba319b386d3896
Akanksha Baishwade <85228361+akanshabaishwade@users.noreply.github.com>
2021-11-18 15:12:53 +0530

    Update settings.py

--- django_project_1/django_project_1/settings.py
+++ django_project_1/django_project_1/settings.py
@@ -17,9 +17,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
